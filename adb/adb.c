dd0e4a37eea74e5362ee12f83b2cb4e0280901f2
apr20th311 <apr20th311@gmail.com>
2015-05-23 00:52:52 +0800

    Merge branch 'cm-12.1' into sm-1.1
    Change-Id: Iccd548a668f0c121dd938c9a12332da5f1cfa1ea

--- adb/adb.c
+++ adb/adb.c
@@ -41,6 +41,8 @@
 #include <sys/prctl.h>
 #include <getopt.h>
 #include <selinux/selinux.h>
+#else
+#include "usb_vendors.h"
 #endif
 
 #if ADB_TRACE
@@ -1316,6 +1318,7 @@ int adb_main(int is_daemon, int server_port)
 #ifdef WORKAROUND_BUG6558362
     if(is_daemon) adb_set_affinity();
 #endif
+    usb_vendors_init();
     usb_init();
     local_init(DEFAULT_ADB_LOCAL_TRANSPORT_PORT);
     adb_auth_init();
@@ -1326,12 +1329,8 @@ int adb_main(int is_daemon, int server_port)
         exit(1);
     }
 #else
-    // Override auth in factory test mode
-    property_get("ro.boot.ftm", value, "0");
-    if (!strcmp(value, "0")) {
-        property_get("ro.adb.secure", value, "0");
-        auth_enabled = !strcmp(value, "1");
-    }
+    property_get("ro.adb.secure", value, "0");
+    auth_enabled = !strcmp(value, "1");
     if (auth_enabled)
         adb_auth_init();
 
@@ -1685,10 +1684,6 @@ int handle_host_request(char *service, transport_type ttype, char* serial, int r
     return -1;
 }
 
-#if !ADB_HOST
-int recovery_mode = 0;
-#endif
-
 int main(int argc, char **argv)
 {
 #if ADB_HOST
@@ -1721,8 +1716,6 @@ int main(int argc, char **argv)
             break;
         }
     }
-
-    recovery_mode = (strcmp(adb_device_banner, "recovery") == 0);
 
     start_device_log();
     D("Handling main()\n");
