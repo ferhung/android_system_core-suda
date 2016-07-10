a05103171558eb2295a801aff3773f08ee49a17b
ferhung-mtk <ferhung27@gmail.com>
2016-07-10 13:39:40 +0800

    MediaTek: fix the mistake of selinux.

--- init/init.cpp
+++ init/init.cpp
@@ -965,8 +965,6 @@ static selinux_enforcing_status selinux_status_from_cmdline() {
 
 static bool selinux_is_disabled(void)
 {
-    return false;  /*return false then set to permissive*/
-
     if (ALLOW_DISABLE_SELINUX) {
         if (access("/sys/fs/selinux", F_OK) != 0) {
             // SELinux is not compiled into the kernel, or has been disabled
@@ -981,6 +979,8 @@ static bool selinux_is_disabled(void)
 
 static bool selinux_is_enforcing(void)
 {
+    return false;  /*return false then set to permissive*/
+
     if (ALLOW_DISABLE_SELINUX) {
         return selinux_status_from_cmdline() == SELINUX_ENFORCING;
     }
